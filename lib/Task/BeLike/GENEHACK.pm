f6e0c4c6f3377bd6d511a6ec258e53cc039174df
John SJ Anderson <genehack@genehack.org>
2011-04-22 18:13:43 -0400

    Initial list

--- lib/Task/BeLike/GENEHACK.pm
+++ lib/Task/BeLike/GENEHACK.pm
@@ -1,7 +1,26 @@
 package Task::BeLike::GENEHACK;
+# ABSTRACT: individuality via conformity
 
 use strict;
 use warnings;
 use 5.010;
+
+=pkgroup Useful Meta Stuff
+
+=pkg Task::Kensho
+
+=pkgroup Apps
+
+=pkg App::Ack
+
+=pkgroup My stuff
+
+=pkg Dist::Zilla::PluginBundle::GENEHACK
+
+=pkg App::MiseEnPlace
+
+=pkg App::GitGot
+
+=cut
 
 1;

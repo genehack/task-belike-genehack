4b12a4d64e1637390491759546b2005624a9d0cc
John SJ Anderson <genehack@genehack.org>
2011-04-22 20:48:18 -0400

    finalize module list ...for now

--- lib/Task/BeLike/GENEHACK.pm
+++ lib/Task/BeLike/GENEHACK.pm
@@ -5,9 +5,59 @@ use strict;
 use warnings;
 use 5.010;
 
-=pkgroup Useful Meta Stuff
+=pkgroup Modules I use
+
+=pkg App::Ack
+
+=pkg App::BashComplete
+
+=pkg App::Cmd
+
+=pkg App::MiseEnPlace
+
+=pkg App::Nopaste
+
+=pkg App::cpanminus
+
+=pkg App::cpanoutdated
+
+=pkg App::perlbrew
+
+=pkg CPAN::Mini
+
+=pkg DBD::SQLite
+
+=pkg Data::Dump::Streamer
+
+=pkg DateTime
+
+=pkg Devel::Cover
+
+=pkg Devel::REPL
+
+=pkg Dist::Zilla::App::Command::cover
+
+=pkg Dist::Zilla::PluginBundle::GENEHACK
+
+=pkg File::Slurp
+
+=pkg List::MoreUtils
 
 =pkg Modern::Perl
+
+=pkg Module::CoreList
+
+=pkg Module::Install
+
+=pkg Module::Which
+
+=pkg MooseX::App::Cmd
+
+=pkg MooseX::Declare
+
+=pkg MooseX::amine
+
+=pkg Perl::Tidy
 
 =pkg Task::Catalyst
 
@@ -27,29 +77,35 @@ use 5.010;
 
 =pkg Task::Kensho::XML
 
-=pkgroup Apps and app frameworks
+=pkg Task::KiokuDB
 
-=pkg App::cpanminus
+=pkg Test::Class
 
-=pkg App::cpanoutdated
+=pkg Test::Exception
 
-=pkg App::Ack
+=pkg Test::MockObject::Extends
 
-=pkg App::Cmd
+=pkg Test::Most
 
-=pkg Devel::REPL
+=pkg Test::Pod
 
-=pkg MooseX::App::Cmd
-
-=pkgroup Useful stuff
+=pkg Test::Pod::Coverage
 
 =pkg Try::Tiny
 
-=pkgroup My stuff
+=pkg XML::LibXML
 
-=pkg App::MiseEnPlace
+=pkg XML::Simple
 
-=pkg Dist::Zilla::PluginBundle::GENEHACK
+=pkg XML::Writer
+
+=pkg YAML
+
+=pkg autodie
+
+=pkg local::lib
+
+=pkg namespace::autospace
 
 =cut
 
